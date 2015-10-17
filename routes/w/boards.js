4fe9e1d01b4b48d30c254db61ddeefd7f2132d1f
Luther Baker <luther@fuzzybearings.com>
2015-10-17 00:08:38 -0500

    Added UPDATE and REMOVE.

--- routes/w/boards.js
+++ routes/w/boards.js
@@ -11,47 +11,68 @@ router.get('/', function(req, res, next) {
 	});
 });
 
-// Get new form
-// - order is important! (before /:board)
-router.get('/edit', function(req, res, next) {
-  res.render('boards/edit', { title: 'Express' });
-});
-
-// Post new board
-router.post('/addboard', function(req, res) {
+router.post('/update', function(req, res) {
 	var db = req.db;
 	var boardName = req.body.boardName;
+	var boardId = req.body.boardId;
 	var boards = db.get('boards');
-	boards.insert({ "name" : boardName }, function(err, doc) {
-		if (err) {
-			res.send("There was a problem adding that board to the database.");
-		}
-		else {
-			res.redirect('/w/boards/');
-		}
-	})
+	if (boardId.length > 1 && boardName.length > 0) {
+		boards.findAndModify({
+			"query" : { "_id" : boardId },
+			"update" : { "name" : boardName },
+			"new" : true,		// no workie?
+			"upsert" : false	// no workie?
+		}, function(err, oldBoard) {
+			boards.findById(boardId, {}, function(e, board) {
+				if (board) {
+					res.render('boards/view', { "board" : board });
+				}
+				else {
+					res.render(page, {});
+				}
+			});
+		});
+	}
+	else {
+		boards.insert({ "name" : boardName }, function(err, board) {
+			if (err) {
+				res.send("There was a problem adding that board to the database.");
+			}
+			else {
+				res.render('boards/view', { "board": board });
+			}
+		});		
+	}
 });
 
-// Board details
 router.get('/:boardId', function(req, res, next) {
 	var db = req.db;
 	var boards = db.get('boards');
 	var boardId = req.params.boardId;
+	var action = req.query.action;
+
+	if (boardId === "0") {
+		res.render('boards/edit', {});
+		return;
+	}
+	
+	if (action === "delete") {
+		boards.remove({ "_id": boardId }, function(err) {
+			res.redirect('/w/boards');
+		});
+		return;
+	}
+
+	var page = 'boards/view';
+	if (action == "edit") {
+		page = 'boards/edit';
+	}
+	
 	boards.findById(boardId, {}, function(e, board) {
 		if (board) {
-			var page = 'boards/board';
-			if (req.query.action === "edit") {
-				page = 'boards/edit';
-			}
-			res.render(page, {
-				"board" : board
-			});
+			res.render(page, { "board" : board });
 		}
-		else {
-			// TODO: no board found! Render a different page!
-			res.render('boards/board', {});
-		}
-	});
+	});	
 });
 
 module.exports = router;
@@ -72,3 +93,34 @@ module.exports = router;
 // 		}
 // 	});
 // });
+
+// else {
+// 	res.redirect('/w/boards/');
+	
+// 	boards.update({ "_id" : boardId }, { "name", boardName });
+// 	boards.findById(boardId, {}, function(e, board) {
+// 		if (board) {
+// 			var page = 'boards/board';
+// 			if (req.query.action === "edit") {
+// 				page = 'boards/edit';
+// 			}
+// 			res.render(page, {
+// 				"board" : board
+// 			});
+// 		}
+// 		else {
+// 			// TODO: no board found! Render a different page!
+// 			res.render('boards/board', {});
+// 		}
+// 	});
+// }
+// else {
+// 	console.log("inserting new!")
+// 	boards.insert({ "name" : boardName }, function(err, doc) {
+// 		if (err) {
+// 			res.send("There was a problem adding that board to the database.");
+// 		}
+// 		else {
+// 			res.redirect('/w/boards/');
+// 		}
+// 	});	
