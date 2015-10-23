9f13375884f3e81f66e98c592be06a837a6c2363
Luther Baker <luther@fuzzybearings.com>
2015-10-23 00:31:19 -0500

    Updated editing columns.

--- routes/w/columns.js
+++ routes/w/columns.js
@@ -1,11 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/:columnId', function(req, res) {
+router.get('/:columnId', function(req, res) {	
 	var db = req.db;
 	var columns = db.get('columns');
 	var columnId = req.params.columnId;
-	columns.findById(columnId, { sort: { "sortOrder" : 1, "name" : 1 }}, function(e, column) {
+	var action = req.query.action;
+	
+	if (columnId === "0") {
+		res.render('columns/edit', {});
+		return;
+	}
+	
+	if (action === "delete") {
+		columns.remove({ "_id" : columnId }, function(err) {
+			columns.findById(columnId, {}, function(e, column) {
+				if (column) {
+					var boards = db.get('boards');
+					boards.findById(column.boardId, {}, function(err, board) {
+						if (err) {
+							console.log("*** ERROR: could not find board (id=" + column.boardId + ") for column.");
+							res.redirect('/w/boards');
+						}
+						else if (board) {
+							res.render('columns/view', { "board" : board, "column" : column });
+						}
+					});
+				}
+				else {
+					res.redirect('/w/boards');
+				}
+			});
+		});
+		return;
+	}
+
+	var page = 'columns/view';
+	if (action == "edit") {
+		page = 'columns/edit';
+	}
+
+	columns.findById(columnId, {}, function(e, column) {
 		if (column) {
 			var boards = db.get('boards');
 			boards.findById(column.boardId, {}, function(err, board) {
@@ -14,15 +49,24 @@ router.get('/:columnId', function(req, res) {
 					res.redirect('/w/boards');
 				}
 				else if (board) {
-					res.render('columns/view', { "board" : board, "column" : column });
+					res.render(page, { "board" : board, "column" : column });
 				}
 			});
-			// res.render('columns/view', { "column" : column });
 		}
 		else {
 			res.redirect('/w/boards');
 		}
 	});
+	
+	// boards.findById(boardId, {}, function(e, board) {
+	// 	var columns = db.get('columns');
+	// 	if (board) {
+	// 		columns.find({ "boardId" : boardId }, { sort : { "sortOrder" : 1 }}, function(e, columns) {
+	// 			res.render(page, { "board" : board, "columns" : columns });
+	// 		});
+	// 	}
+	// });
+	
 });
 
 router.post('/update', function(req, res, next) {
@@ -30,22 +74,18 @@ router.post('/update', function(req, res, next) {
 	var db = req.db;
 	
 	// boards
-	var boardName = req.body.boardName;
 	var boardId = req.body.boardId;
 	var columnsTable = db.get('columns');
 	
 	// column
-	columnsTable.find({ "boardId" : boardId }, { "sort" : "sortOrder" }, function(err, columns) {
+	columnsTable.find({ "boardId" : boardId }, { sort : { "sortOrder" : 1 }}, function(err, columns) {
 	
 		var columnName = req.body.columnName;
 	
 		if (req.body.columnId && req.body.columnId.length > 1) {
 			var columnId = req.body.columnId;
-			if (req.body.columnName && req.body.columnName.length > 0) {
-			
-				var columnId = req.body.columnId;
+			if (req.body.columnName && req.body.columnName.length > 0) {			
 				var sortOrder = parseInt(req.body.sortOrder);
-			
 				columnsTable.findAndModify({
 					"query" : { "_id" : columnId },
 					"update" : { "boardId" : boardId, "name" : columnName, "sortOrder" : sortOrder },
@@ -57,7 +97,24 @@ router.post('/update', function(req, res, next) {
 						res.redirect('/w/boards');					
 					}
 					else {
-						refreshBoardsEdit(db, boardId, res);
+						columnsTable.findById(columnId, {}, function(e, column) {
+							if (column) {
+								var boards = db.get('boards');
+								boards.findById(column.boardId, {}, function(err, board) {
+									if (err) {
+										console.log("*** ERROR: could not find board (id=" + column.boardId + ") for column.");
+										res.redirect('/w/boards');
+									}
+									else if (board) {
+										res.render('columns/view', { "board" : board, "column" : column });
+									}
+								});
+							}
+							else {
+								res.redirect('/w/boards');
+							}
+						});
+						// refreshBoardsEdit(db, boardId, res);
 					}
 				});
 			}
--- start-application.sh
+++ start-application.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-npm start
+// npm start
+
+npm run-script debug
+
