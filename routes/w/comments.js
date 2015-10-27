44a9d36b37aa309f0be18fa342b196daa87df7e7
Luther Baker <luther@fuzzybearings.com>
2015-10-27 03:34:34 -0500

    Added comments.

---
+++ routes/w/comments.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var router = express.Router();
+var sharedRoutes = require('./sharedRoutes');
+
+router.get('/:docId', function(req, res) {	
+	var docId = req.params.docId;
+	sharedRoutes.renderDocumentPageColumn(req, res, docId);
+});
+
+router.post('/update', function(req, res) {
+	
+	var db = req.db;
+	var docName = req.body.name;
+	var sortOrder = req.body.sortOrder;
+	var cardId = req.body.cardId;
+	var docId = req.body.columnId;
+	var docsTable = db.get('comments');
+
+	if (docId.length > 1) {
+		if (docName && docName.length > 0) {
+			docsTable.findAndModify({
+				"query" : { "_id" : docId },
+				"update" : { "name" : docName, "sortOrder" : sortOrder, "cardId" : cardId },
+				"new" : true,		// no workie?
+				"upsert" : false	// no workie?
+			}, function(err, oldDoc) {
+				sharedRoutes.renderDocumentPageComment(req, res, docId);
+			});			
+		}
+		else {
+			docsTable.remove({ "_id" : docId }, function(err) {
+				if (err) {
+					res.send("There was a problem removing that document from the database.");					
+				}
+				else {
+					sharedRoutes.renderDocumentPageComment(req, res, null);					
+				}
+			});			
+		}
+	}
+	else if (docName && docName.length > 0) {
+		docsTable.insert({ "name" : docName, "sortOrder" : sortOrder, "cardId" : cardId }, function(err, doc) {
+			if (doc) {
+				sharedRoutes.renderDocumentPageComment(req, res, doc._id);
+			}
+			else {
+				res.send("There was a problem adding that document to the database.");
+			}
+		});
+	}
+	else {
+		sharedRoutes.renderDocumentPageComment(req, res, null);		
+	}
+});
+
+module.exports = router;
