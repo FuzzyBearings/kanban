3ca94b3fc50762c9fb61c7a7c46bd426b79b7b65
Luther Baker <luther@fuzzybearings.com>
2015-10-30 02:16:30 -0500

    Added kanban path.

--- routes/w/sharedRoutes.js
+++ routes/w/sharedRoutes.js
@@ -1,5 +1,9 @@
-var editorPage = 'portrait';
-var kanbanPage = 'kanban';
+// var editorPage = 'editor/portrait1';
+// var kanbanPage = 'editor/portrait2';
+// var editorPage = 'viewer/portrait';
+// var kanbanPage = 'viewer/portrait';
+var editorPage = 'kanban/kanban';
+var kanbanPage = 'kanban/kanban';
 
 function renderDocumentPageComment(req, res, commentId) {
 	res.targetPage = kanbanPage;
