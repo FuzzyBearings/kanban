2af8cb6b83b178c588bcdfe3a4978f2fefb57305
Luther Baker <luther@lutherbaker.com>
2015-11-08 00:37:05 -0600

    Happier triggers for editing columns and cards.

--- routes/w/sharedRoutes.js
+++ routes/w/sharedRoutes.js
@@ -1,7 +1,7 @@
 var async = require('async');
 
 var editorPage = 'bootstrap/editor';
-var kanbanPage = 'jquery/kanban';
+var kanbanPage = 'bootstrap/kanban';
 
 function renderDocumentPageComment(req, res, commentId) {
 	res.targetPage = kanbanPage;
