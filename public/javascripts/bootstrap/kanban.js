d5033f4c718d99b04a141e29171906cb9d786bcd
Luther Baker <luther@lutherbaker.com>
2015-11-11 13:33:06 -0600

    Worked on the jQueryUI sortable callbacks.

--- public/javascripts/bootstrap/kanban.js
+++ public/javascripts/bootstrap/kanban.js
@@ -1,9 +1,44 @@
+
+// http://stackoverflow.com/questions/1601827/jquery-ui-sortable-how-to-determine-current-location-and-new-location-in-update
+// http://stackoverflow.com/questions/16082519/updating-sort-order-during-sort-change-event-jquery-ui
+
+var dict = { };
 $(function() {
 	$('ul.sortable').sortable({
 		connectWith: '.sortable',
 		placeholder: "card-placeholder",
 		revert: true,
-		forcePlaceholderSize: true
+		forcePlaceholderSize: true,
+		remove: function(event, ui) {
+			dict.source = event.target;
+			// console.log("REMOVED! from list " + event.target);
+		},
+		receive: function(event, ui) {
+			dict.destination = event.target;
+			// console.log("RECEIVED! into list " + event.target);
+		},
+		start: function(event, ui) {
+			dict.start = ui.item.index();
+			dict.stop = null;
+			dict.source = null;
+			dict.destination = null;
+			// console.log('STARTED: index(' + ui.item.index() + ') sender(' + ui.sender + ')');
+		},
+		stop: function(event, ui) {
+			if (dict.source && dict.destination) {
+				console.log('source { column: ' + dict.source.id + ', sortOrder: ' + dict.start + ' } destination { column: ' + dict.destination.id + ', sortOrder: ' + dict.stop + '}');
+			}
+			else {
+				console.log('(' + dict.start + ') ' + '(' + dict.stop + ')');
+			}
+			// console.log('STOPPED: index(' + ui.item.index() + ') sender(' + ui.sender + ')');
+		},
+		update: function(event, ui) {
+			dict.stop = ui.item.index();
+			// var sender = ui.sender;
+			// sender = sender ? sender.get(0) : null;
+			// console.log('UPDATED: index(' + ui.item.index() + ') sender(' + ui.sender + ')');
+		}
 	});
 	$('#cardModal').on('show.bs.modal', function (event) {
 		
