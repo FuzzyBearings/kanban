0946c9a5ea2678b3433f7900863d3e7f55219a1a
Luther Baker <luther@lutherbaker.com>
2015-11-27 23:05:15 -0600

    Updated button order and names.

--- public/javascripts/bootstrap/kanban.js
+++ public/javascripts/bootstrap/kanban.js
@@ -2,7 +2,7 @@
 // http://stackoverflow.com/questions/1601827/jquery-ui-sortable-how-to-determine-current-location-and-new-location-in-update
 // http://stackoverflow.com/questions/16082519/updating-sort-order-during-sort-change-event-jquery-ui
 
-var dict = { };
+var cardDict = { };
 $(function() {
 	$('ul.sortable').sortable({
 		connectWith: '.sortable',
@@ -10,29 +10,29 @@ $(function() {
 		revert: true,
 		forcePlaceholderSize: true,
 		remove: function(event, ui) {
-			dict.source = event.target;
+			cardDict.source = event.target;
 			// console.log("REMOVED! from list " + event.target);
 		},
 		receive: function(event, ui) {
-			dict.destination = event.target;
+			cardDict.destination = event.target;
 			// console.log("RECEIVED! into list " + event.target);
 		},
 		start: function(event, ui) {
-			dict.source = event.target;
-			dict.destination = event.target;
+			cardDict.source = event.target;
+			cardDict.destination = event.target;
 			// console.log('STARTED! for list ' + event.target);
-			dict.start = ui.item.index();
-			dict.stop = 0;
-			dict.update = false;
+			cardDict.start = ui.item.index();
+			cardDict.stop = 0;
+			cardDict.update = false;
 			// console.log('STARTED: index(' + ui.item.index() + ') sender(' + ui.sender + ')');
 		},
 		stop: function(event, ui) {
-			if (dict.update) {
-				var columnId = dict.destination.id;
+			if (cardDict.update) {
+				var columnId = cardDict.destination.id;
 				var cardId = ui.item.get(0).id;
-				var json = { cardId: cardId, columnId: columnId, startIndex: parseInt(dict.start), stopIndex: parseInt(dict.stop) };
+				var json = { cardId: cardId, columnId: columnId, startIndex: parseInt(cardDict.start), stopIndex: parseInt(cardDict.stop) };
 				var url = "/cards/" + cardId;
-				// console.log('STOPPED! card.id: ' + cardId + ', source { column: ' + dict.source.id + ', sortOrder: ' + dict.start + ' } destination { column: ' + columnId + ', sortOrder: ' + dict.stop + '}');
+				// console.log('STOPPED! card.id: ' + cardId + ', source { column: ' + cardDict.source.id + ', sortOrder: ' + cardDict.start + ' } destination { column: ' + columnId + ', sortOrder: ' + cardDict.stop + '}');
 
 				var jqXHR = $.ajax({
 					type: "PUT",
@@ -52,8 +52,8 @@ $(function() {
 			}
 		},
 		update: function(event, ui) {
-			dict.update = true;
-			dict.stop = ui.item.index();
+			cardDict.update = true;
+			cardDict.stop = ui.item.index();
 			// var sender = ui.sender;
 			// sender = sender ? sender.get(0) : null;
 			// console.log('UPDATED: index(' + ui.item.index() + ') sender(' + ui.sender + ')');
