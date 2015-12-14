223d9b565ca9a44165925607024198ce7681ee59
Luther Baker <luther@lutherbaker.com>
2015-12-14 07:51:19 -0600

    Updates.

--- public/javascripts/bootstrap/kanban.js
+++ public/javascripts/bootstrap/kanban.js
@@ -45,7 +45,8 @@ drake.on('drop', function(element, target, source, sibling) {
 });
 
 function showModal(cardId, cardName, cardDesc) {
-	console.log('cardId: ' + cardId + ', cardName: ' + cardName + ', cardDesc: ' + cardDesc);
+	console.log('cardId: ' + cardId + ', cardName: ' + cardName + ', cardDesc: ' + cardDesc);	
+	// var modal = $(this)
 	$('#modalCardName').val(cardName);
 	$('#modalCardDescription').val(cardDesc);
 	$('#cardModal').modal({});
@@ -111,24 +112,30 @@ $(function() {
 			// console.log('UPDATED: index(' + ui.item.index() + ') sender(' + ui.sender + ')');
 		}
 	});
-	$('#cardModal').on('show.bs.modal', function (event) {
-		
-		var button = $(event.relatedTarget);
-		var cardId = button.data('cardid');
-		var cardSortOrder = button.data('cardsortorder');
-		var cardName = button.data('name');
-		var columnId = button.data('columnid');
+});
 
-		// var msg = cardId + ", " + cardSortOrder + ", " + cardName + ", " + columnId;
-		// alert(msg);
+//
+// boostrap
+//
+$('#cardModal').on('show.bs.modal', function (event) {
+	
+	var button = $(event.relatedTarget);
+	var cardId = button.data('cardid');
+	var cardSortOrder = button.data('cardsortorder');
+	var cardName = button.data('name');
+	var columnId = button.data('columnid');
 
-		var modal = $(this);
-		
-	  // var button = $(event.relatedTarget) // Button that triggered the modal
-	  // var recipient = button.data('whatever') // Extract info from data-* attributes
-	  // // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-	  // // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-	  // modal.find('.modal-title').text('New message to ' + recipient)
-	  // modal.find('.modal-body input').val(recipient)
-	})
+	var msg = cardId + ", " + cardSortOrder + ", " + cardName + ", " + columnId;
+	console.log(msg);
+
+	var modal = $(this);
+	
+
+	
+  // var button = $(event.relatedTarget) // Button that triggered the modal
+  // var recipient = button.data('whatever') // Extract info from data-* attributes
+  // // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  // modal.find('.modal-title').text('New message to ' + recipient)
+  // modal.find('.modal-body input').val(recipient)
 });
